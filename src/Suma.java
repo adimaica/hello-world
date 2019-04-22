ea14973eb6c0142eca05c35d6c1268f2916bca49
adimaica <adimaica16@gmail.com>
2019-04-22 15:53:48 +0300

    Suma2

--- src/Suma.java
+++ src/Suma.java
@@ -2,7 +2,7 @@ public class Suma {
 
     public static void main(String[] args) {
 
-        int num1 = 5, num2 = 15, sum;
+        int num1 = 25, num2 = 15, sum;
         sum = num1 + num2;
 
         System.out.println("Suma a doua numere: "+sum);
